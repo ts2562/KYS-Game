8f07dc72ccc556a81964e3e1d7b1686b58133f4e
Sqwerrels <kp1572@nyu.edu>
2017-03-29 12:43:39 -0400

    Level 2 fixes

---
+++ Assets/Scripts/lvl1scripts/SpikeScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeScript : MonoBehaviour {
+
+	public GameObject Player;
+	public GameObject Life1;
+	public AudioSource audio;
+	Vector3 start = new Vector3(-28.1f, 0.4911492f, 0);
+	Vector3 playerPos;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		playerPos = new Vector3(Player.transform.position.x,Player.transform.position.y,0);
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+     	if(col.gameObject.name == "Player"){
+     		//Destroy(col.gameObject);
+     		audio.Play();
+     		Player.transform.position = start;
+     		Life1.transform.position = playerPos;
+     	}
+     }
+}
---
+++ Assets/Scripts/lvl3scripts/SpeedPlatformScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedPlatformScript : MonoBehaviour {
+
+	public GameObject Player;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+     
+	/*
+	void OnTriggerEnter2D(Collider2D col){
+		if(col.gameObject.name == "Player"){
+			//playerScript.speed = 26.0f;
+			GameObject.Find("Player").GetComponent<PlayerScript>().speed = 45.0f;
+			//Debug.Log ("1");
+		}
+	}
+*/
+}

e010e0e4b4d574a755df179615bd85af6134b37c
Timothy Seid <ts2562@nyu.edu>
2017-04-24 21:53:36 -0400

    Added New Level
    Cannot Jump On Ice Platforms

---
+++ Assets/Scripts/PlatMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using System;
+
+public class PlatMove : MonoBehaviour 
+{
+float originalY;
+
+public float floatStrength = 30;
+int flo = 0;
+
+
+void Start ()
+{
+    originalY = transform.position.y;
+}
+
+void Update () {
+    transform.position = new Vector3(transform.position.x, originalY + (float)(Math.Sin(Time.time) * (25)), 0);
+}
+
+}

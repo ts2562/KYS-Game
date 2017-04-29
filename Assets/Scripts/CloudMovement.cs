e9371a6dfe312b4949df45f17c5b9d4fcf1fb8c6
bunnyliao <zl1441@nyu.edu>
2017-04-29 14:02:33 -0400

    Resources_1

---
+++ Assets/Scripts/CloudMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMovement : MonoBehaviour 
+{
+
+	public float moveSpeed;
+	public float minX;
+	public float maxX;
+	private Transform[] cloudTr;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		cloudTr = new Transform[this.transform.childCount];
+
+		for (int i = 0; i < this.transform.childCount; i++) 
+		{
+			cloudTr [i] = this.transform.GetChild (i);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		for (int i = 0; i < cloudTr.Length; i++) 
+		{
+			cloudTr [i].position = new Vector3 (cloudTr[i].position.x - moveSpeed, cloudTr[i].position.y, 0);
+			if(cloudTr[i].position.x < minX)
+			{
+				cloudTr [i].position = new Vector3 (maxX, cloudTr[i].position.y, 0);
+			}
+		}
+	}
+}
